12efc9c5c6068afba5e02f95c2a413e85481d707
odevpedro <pedrosschmidt2@gmail.com>
2024-07-29 21:02:23 -0300

    primeiro componente

--- meu-primeiro/README.md
+++ meu-primeiro/README.md
@@ -1,4 +1,4 @@
-# PrimeiroProjeto
+# MeuPrimeiro
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.2.
 
--- meu-primeiro/src/app/app.component.spec.ts
+++ meu-primeiro/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'primeiro-projeto' title`, () => {
+  it(`should have the 'meu-primeiro' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('primeiro-projeto');
+    expect(app.title).toEqual('meu-primeiro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, primeiro-projeto');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, meu-primeiro');
   });
 });
